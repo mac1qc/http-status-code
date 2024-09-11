6926dabf6d20227baae9ec88fd12f2350fd65bf2
Martin <martin@1696.io>
2024-09-11 19:22:02 -0400

    Add more status code

--- src/StatusCode.php
+++ src/StatusCode.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace MartinAC;
 
-use Exception;
-
 enum StatusCode: int
 {
     case CONTINUE               = 100;
@@ -34,7 +32,6 @@ enum StatusCode: int
 
     case MULTIPLE_CHOICES   = 300;
     case MOVED_PERMAMENTLY  = 301;
-    case MOVED_TEMPORARILY  = 302;
     case FOUND              = 302;
     case SEE_OTHER          = 303;
     case NOT_MODIFIED       = 304;
@@ -42,122 +39,189 @@ enum StatusCode: int
     case SWITCH_PROXY       = 306;
     case TEMPORARY_REDIRECT = 307;
     case PERMANENT_REDIRECT = 308;
-    /*
-    400 Bad Request
-    401 Unauthorized
-    402 Payment Required
-    403 Forbidden
-    404 Not Found
-    405 Method Not Allowed
-    406 Not Acceptable
-    407 Proxy Authentication Required
-    408 Request Timeout
-    409 Conflict
-    410 Gone
-    411 Length Required
-    412 Precondition Failed
-    413 Payload Too Large
-    414 URI Too Long
-    415 Unsupported Media Type
-    416 Range Not Satisfiable
-    417 Expectation Failed
-    418 I'm a teapot (RFC 2324, RFC 7168)
-    419 Page Expired (Laravel Framework)
-    420 Method Failure (Spring Framework)
-    420 Enhance Your Calm
-    421 Misdirected Request
-    422 Unprocessable Content
-    423 Locked (WebDAV; RFC 4918)
-    424 Failed Dependency (WebDAV; RFC 4918)
-    425 Too Early (RFC 8470)
-    426 Upgrade Required
-    428 Precondition Required (RFC 6585)
-    429 Too Many Requests (RFC 6585)
-    430 Request Header Fields Too Large (Shopify)
-    430 Shopify Security Rejection (Shopify)
-    431 Request Header Fields Too Large (RFC 6585)
-    440 Login Time-out
-    444 No Response
-    449 Retry With
-    450 Blocked by Windows Parental Controls
-    451 Unavailable For Legal Reasons (RFC 7725)
-    451 Redirect
-    494 Request header too large
-    495 SSL Certificate Error
-    496 SSL Certificate Required
-    497 HTTP Request Sent to HTTPS Port
-    498 Invalid Token (Esri)
-    499 Client Closed Request
-    499 Token Required (Esri)
 
-    500 Internal Server Error
-    501 Not Implemented
-    502 Bad Gateway
-    503 Service Unavailable
-    504 Gateway Timeout
-    505 HTTP Version Not Supported
-    506 Variant Also Negotiates (RFC 2295)
-    507 Insufficient Storage (WebDAV; RFC 4918)
-    508 Loop Detected (WebDAV; RFC 5842)
-    509 Bandwidth Limit Exceeded
-    510 Not Extended (RFC 2774)
-    511 Network Authentication Required (RFC 6585)
-    520 Web Server Returned an Unknown Error
-    521 Web Server Is Down
-    522 Connection Timed Out
-    523 Origin Is Unreachable
-    524 A Timeout Occurred
-    525 SSL Handshake Failed
-    526 Invalid SSL Certificate
-    527 Railgun Error (obsolete)
-    529 Site is overloaded
-    530 Site is frozen
-    530 Origin DNS Error (Shopify)
-    540 Temporarily Disabled (Shopify)
-    561 Unauthorized
-    598 (Informal convention) Network read timeout error
-    599 Network Connect Timeout Error
+    case BAD_REQUEST                       = 400;
+    case UNAUTHORIZED                      = 401;
+    case PAYMENT_REQUIRED                  = 402;
+    case FORBIDDEN                         = 403;
+    case NOT_FOUND                         = 404;
+    case METHOD_NOT_ALLOWED                = 405;
+    case NOT_ACCEPTABLE                    = 406;
+    case PROXY_AUTHENTICATION_REQUIRED     = 407;
+    case REQUEST_TIMEOUT                   = 408;
+    case CONFLICT                          = 409;
+    case GONE                              = 410;
+    case LENGTH_REQUIRED                   = 411;
+    case PRECONDITION_FAILED               = 412;
+    case PAYLOAD_TOO_LARGE                 = 413;
+    case URI_TOO_LONG                      = 414;
+    case UNSUPPORTED_MEDIA_TYPE            = 415;
+    case RANGE_NOT_SATISFIABLE             = 416;
+    case EXPECTATION_FAILED                = 417;
+    case TEAPOT                            = 418;
+    case PAGE_EXPIRED                      = 419;
+    case METHOD_FAILURE                    = 420;
+    case ENHANCE_YOUR_CALM                 = 420;
+    case MISDIRECTED_REQUEST               = 421;
+    case UNPROCESSABLE_CONTENT             = 422;
+    case LOCKED                            = 423;
+    case FAILED_DEPENDENCY                 = 424;
+    case TOO_EARLY                         = 425;
+    case UPGRADE_REQUIRED                  = 426;
+    case PRECONDITION_REQUIRED             = 428;
+    case TOO_MANY_REQUESTS                 = 429;
+    case REQUESTS_HEADER_FIELDS_TOO_LARGE  = 430;
+    case REQUEST_HEADER_FIELDS_TOO_LARGE   = 431;
+    case LOGIN_TIME_OUT                    = 440;
+    case NO_RESPONSE                       = 444;
+    case RETRY_WITH                        = 449;
+    case BLOCKED_WINDOWS_PARENTAL_CONTROLS = 450;
+    case UNAVAILABLE_LEGAL_REASONS         = 451;
+    case REDIRECT                          = 451;
+    case REQUEST_HEADER_TOO_LARGE          = 494;
+    case SSL_CERTIFICATE_ERROR             = 495;
+    case SSL_CERTIFICATE_REQUIRED          = 496;
+    case HTTP_REQUEST_SENT_TO_HTTPS_PORT   = 497;
+    case INVALID_TOKEN                     = 498;
+    case TOKEN_REQUIRED                    = 499;
 
-    783 Unexpected Token (Shopify)
+    case INTERNAL_SERVER_ERROR             = 500;
+    case NOT_IMPLEMENTED                   = 501;
+    case BAD_GATEWAY                       = 502;
+    case SERVICE_UNAVAILABLE               = 503;
+    case GATEWAY_TIMEOUT                   = 504;
+    case HTTP_VERSION_NOT_SUPPORTED        = 505;
+    case VARIANT_ALSO_NEGOTIATES           = 506;
+    case INSUFFICIENT_STORAGE              = 507;
+    case LOOP_DETECTED                     = 508;
+    case BANDWITH_LIMIT_EXCEEDED           = 509;
+    case NOT_EXTENDED                      = 510;
+    case NETWORK_AUTHENTICATION_REQUIRED   = 511;
+    case WEB_SERVER_RETURNED_UNKNOWN_ERROR = 520;
+    case WEB_SERVER_DOWN                   = 521;
+    case CONNECTION_TIME_OUT               = 522;
+    case ORIGIN_UNREACHABLE                = 523;
+    case TIMEOUT_OCCURRED                  = 524;
+    case SSL_HANDSHAKE_FAILED              = 525;
+    case INVALID_SSL_CERTIFICATE           = 526;
+    case RAILGUN_ERROR                     = 527;
+    case SITE_OVERLOADED                   = 529;
+    case ORIGIN_DNS_ERROR                  = 530;
+    case TEMPORARILY_DISABLED              = 540;
+    case SERVER_UNAUTHORIZED               = 561;
+    case NETWORK_READ_TIMEOUT_ERROR        = 598;
+    case NETWORK_CONNECT_TIMEOUT_ERROR     = 599;
 
-    999 Non-standard
-    */
+    case UNEXPECTED_TOKEN = 783;
+
+    case NON_STANDARD = 999;
 
     public static function getLabel(self $value): string
     {
         return match ($value) {
-            self::CONTINUE                         => 'Continue',
-            self::SWITCHING_PROTOCOL               => 'Switching Protocols',
-            self::PROCESSING                       => 'Processing',
-            self::EARLY_HINTS                      => 'Early Hints',
-            self::RESPONSE_STALE                   => 'Response is Stale',
-            self::REVILIDATION_FAILED              => 'Revalidation Failed',
-            self::DISCONNECTED_OPERATION           => 'Disconnected Operation',
-            self::HEURISTIC_EXPIRATION             => 'Heuristic Expiration',
-            self::MISCELLANOUS_WARNING             => 'Miscellaneous Warning',
-            self::OK                               => 'OK',
-            self::CREATED                          => 'Created',
-            self::ACCEPTED                         => 'Accepted',
-            self::NON_AUTHORITATIVE                => 'Non-Authoritative Information',
-            self::NO_CONTENT                       => 'No Content',
-            self::RESET_CONTENT                    => 'Reset Content',
-            self::PARTIAL_CONTENT                  => 'Partial Content',
-            self::MULTI_STATUS                     => 'Multi-Status',
-            self::ALREADY_REPORTED                 => 'Already Reported',
-            self::TRANSFORMATION_APPLIED           => 'Transformation Applied',
-            self::THIS_IS_FINE                     => 'This is fine',
-            self::IM_USED                          => 'IM Used',
-            self::MISCELLANEOUS_PERSISTENT_WARNING => 'Miscellaneous Persistent Warning',
-            self::MULTIPLE_CHOICES                 => 'Multiple Choices',
-            self::MOVED_PERMAMENTLY                => 'Moved Permanently',
-            self::MOVED_TEMPORARILY                => 'Moved temporarily',
-            self::FOUND                            => 'Found',
-            self::SEE_OTHER                        => 'See Other',
-            self::NOT_MODIFIED                     => 'Not Modified',
-            self::USE_PROXY                        => 'Use Proxy',
-            self::SWITCH_PROXY                     => 'Switch Proxy',
-            self::TEMPORARY_REDIRECT               => 'Temporary Redirect',
-            self::PERMANENT_REDIRECT               => 'Permanent Redirect',
+            self::CONTINUE                          => 'Continue',
+            self::SWITCHING_PROTOCOL                => 'Switching Protocols',
+            self::PROCESSING                        => 'Processing',
+            self::EARLY_HINTS                       => 'Early Hints',
+            self::RESPONSE_STALE                    => 'Response is Stale',
+            self::REVILIDATION_FAILED               => 'Revalidation Failed',
+            self::DISCONNECTED_OPERATION            => 'Disconnected Operation',
+            self::HEURISTIC_EXPIRATION              => 'Heuristic Expiration',
+            self::MISCELLANOUS_WARNING              => 'Miscellaneous Warning',
+            self::OK                                => 'OK',
+            self::CREATED                           => 'Created',
+            self::ACCEPTED                          => 'Accepted',
+            self::NON_AUTHORITATIVE                 => 'Non-Authoritative Information',
+            self::NO_CONTENT                        => 'No Content',
+            self::RESET_CONTENT                     => 'Reset Content',
+            self::PARTIAL_CONTENT                   => 'Partial Content',
+            self::MULTI_STATUS                      => 'Multi-Status',
+            self::ALREADY_REPORTED                  => 'Already Reported',
+            self::TRANSFORMATION_APPLIED            => 'Transformation Applied',
+            self::THIS_IS_FINE                      => 'This is fine',
+            self::IM_USED                           => 'IM Used',
+            self::MISCELLANEOUS_PERSISTENT_WARNING  => 'Miscellaneous Persistent Warning',
+            self::MULTIPLE_CHOICES                  => 'Multiple Choices',
+            self::MOVED_PERMAMENTLY                 => 'Moved Permanently',
+            self::FOUND                             => 'Found',
+            self::SEE_OTHER                         => 'See Other',
+            self::NOT_MODIFIED                      => 'Not Modified',
+            self::USE_PROXY                         => 'Use Proxy',
+            self::SWITCH_PROXY                      => 'Switch Proxy',
+            self::TEMPORARY_REDIRECT                => 'Temporary Redirect',
+            self::PERMANENT_REDIRECT                => 'Permanent Redirect',
+            self::BAD_REQUEST                       => 'Bad Request',
+            self::UNAUTHORIZED                      => 'Unauthorized',
+            self::PAYMENT_REQUIRED                  => 'Payment Required',
+            self::FORBIDDEN                         => 'Forbidden',
+            self::NOT_FOUND                         => 'Not Found',
+            self::METHOD_NOT_ALLOWED                => 'Method Not Allowed',
+            self::NOT_ACCEPTABLE                    => 'Not Acceptable',
+            self::PROXY_AUTHENTICATION_REQUIRED     => 'Proxy Authentication Required',
+            self::REQUEST_TIMEOUT                   => 'Request Timeout',
+            self::CONFLICT                          => 'Conflict',
+            self::GONE                              => 'Gone',
+            self::LENGTH_REQUIRED                   => 'Length Required',
+            self::PRECONDITION_FAILED               => 'Precondition Failed',
+            self::PAYLOAD_TOO_LARGE                 => 'Payload Too Large',
+            self::URI_TOO_LONG                      => 'URI Too Long',
+            self::UNSUPPORTED_MEDIA_TYPE            => 'Unsupported Media Type',
+            self::RANGE_NOT_SATISFIABLE             => 'Range Not Satisfiable',
+            self::EXPECTATION_FAILED                => 'Expectation Failed',
+            self::TEAPOT                            => 'I\'m a teapot',
+            self::PAGE_EXPIRED                      => 'Page Expired',
+            self::METHOD_FAILURE                    => 'Method Failure',
+            self::ENHANCE_YOUR_CALM                 => 'Enhance Your Calm',
+            self::MISDIRECTED_REQUEST               => 'Misdirected Request',
+            self::UNPROCESSABLE_CONTENT             => 'Unprocessable Content',
+            self::LOCKED                            => 'Locked',
+            self::FAILED_DEPENDENCY                 => 'Failed Dependency',
+            self::TOO_EARLY                         => 'Too Early',
+            self::UPGRADE_REQUIRED                  => 'Upgrade Required',
+            self::PRECONDITION_REQUIRED             => 'Precondition Required',
+            self::TOO_MANY_REQUESTS                 => 'Too Many Requests',
+            self::REQUESTS_HEADER_FIELDS_TOO_LARGE  => 'Requests Header Fields Too Large',
+            self::REQUEST_HEADER_FIELDS_TOO_LARGE   => 'Request Header Fields Too Large',
+            self::LOGIN_TIME_OUT                    => 'Login Time-out',
+            self::NO_RESPONSE                       => 'No Response',
+            self::RETRY_WITH                        => 'Retry With',
+            self::BLOCKED_WINDOWS_PARENTAL_CONTROLS => 'Blocked by Windows Parental Controls',
+            self::UNAVAILABLE_LEGAL_REASONS         => 'Unavailable For Legal Reasons',
+            self::REDIRECT                          => 'Redirect',
+            self::REQUEST_HEADER_TOO_LARGE          => 'Request header too large',
+            self::SSL_CERTIFICATE_ERROR             => 'SSL Certificate Error',
+            self::SSL_CERTIFICATE_REQUIRED          => 'SSL Certificate Required',
+            self::HTTP_REQUEST_SENT_TO_HTTPS_PORT   => 'HTTP Request Sent to HTTPS Port',
+            self::INVALID_TOKEN                     => 'Invalid Token',
+            self::TOKEN_REQUIRED                    => 'Token Required',
+            self::INTERNAL_SERVER_ERROR             => 'Internal Server Error',
+            self::NOT_IMPLEMENTED                   => 'Not Implemented',
+            self::BAD_GATEWAY                       => 'Bad Gateway',
+            self::SERVICE_UNAVAILABLE               => 'Service Unavailable',
+            self::GATEWAY_TIMEOUT                   => 'Gateway Timeout',
+            self::HTTP_VERSION_NOT_SUPPORTED        => 'HTTP Version Not Supported',
+            self::VARIANT_ALSO_NEGOTIATES           => 'Variant Also Negotiates',
+            self::INSUFFICIENT_STORAGE              => 'Insufficient Storage',
+            self::LOOP_DETECTED                     => 'Loop Detected',
+            self::BANDWITH_LIMIT_EXCEEDED           => 'Bandwidth Limit Exceeded',
+            self::NOT_EXTENDED                      => 'Not Extended',
+            self::NETWORK_AUTHENTICATION_REQUIRED   => 'Network Authentication Required',
+            self::WEB_SERVER_RETURNED_UNKNOWN_ERROR => 'Web Server Returned an Unknown Error',
+            self::WEB_SERVER_DOWN                   => 'Web Server Is Down',
+            self::CONNECTION_TIME_OUT               => 'Connection Timed Out',
+            self::ORIGIN_UNREACHABLE                => 'Origin Is Unreachable',
+            self::TIMEOUT_OCCURRED                  => 'A Timeout Occurred',
+            self::SSL_HANDSHAKE_FAILED              => 'SSL Handshake Failed',
+            self::INVALID_SSL_CERTIFICATE           => 'Invalid SSL Certificate',
+            self::RAILGUN_ERROR                     => 'Railgun Error',
+            self::SITE_OVERLOADED                   => 'Site is overloaded',
+            self::ORIGIN_DNS_ERROR                  => 'Origin DNS Error',
+            self::TEMPORARILY_DISABLED              => 'Temporarily Disabled',
+            self::SERVER_UNAUTHORIZED               => 'Unauthorized',
+            self::NETWORK_READ_TIMEOUT_ERROR        => 'Network read timeout error',
+            self::NETWORK_CONNECT_TIMEOUT_ERROR     => 'Network Connect Timeout Error',
+            self::UNEXPECTED_TOKEN                  => 'Unexpected Token',
+            self::NON_STANDARD                      => 'Non-standard',
         };
     }
 }
